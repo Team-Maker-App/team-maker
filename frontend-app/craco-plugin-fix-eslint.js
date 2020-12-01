338a562c44e8bb02ce2e209f6ef121690c064fc1
Hernan Casé <her.case@live.com.ar>
2020-12-01 18:50:53 -0300

    add tailwindcss and basic configurations (#1)
    * add tailwindcss
    * add postcss
    * add prettier config

---
+++ frontend-app/craco-plugin-fix-eslint.js
@@ -0,0 +1,20 @@
+module.exports = {
+  overrideCracoConfig: ({ cracoConfig }) => {
+    if (typeof cracoConfig.eslint.enable !== "undefined") {
+      cracoConfig.disableEslint = !cracoConfig.eslint.enable;
+    }
+    delete cracoConfig.eslint;
+    return cracoConfig;
+  },
+  overrideWebpackConfig: ({ webpackConfig, cracoConfig }) => {
+    if (
+      typeof cracoConfig.disableEslint !== "undefined" &&
+      cracoConfig.disableEslint === true
+    ) {
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        (instance) => instance.constructor.name !== "ESLintWebpackPlugin"
+      );
+    }
+    return webpackConfig;
+  },
+};
--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">TeamMaker</h3>
+      </div>
     </div>
   );
 }
