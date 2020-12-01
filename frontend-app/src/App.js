ec873fc4b379b2550009d03227a99dd2dc9e9461
Hernan Case <her.case@live.com.ar>
2020-12-01 18:49:09 -0300

    add configurations

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">
-          Job Postings
-        </h3>
+        <h3 class="text-lg leading-6 font-medium text-gray-900">TeamMaker</h3>
       </div>
     </div>
   );
