f6233d308f413841583159860b50185fb6d669fb
Hernan Case <her.case@live.com.ar>
2020-12-15 20:39:13 -0300

    add Layout component

--- frontend-app/src/screens/ListTeam/List.js
+++ frontend-app/src/screens/ListTeam/List.js
@@ -5,17 +5,15 @@ import { ReactComponent as TShirtBlue } from "../../styles/svg/blueTShirt.svg";
 import Alert from "../../components/Alert/Alert";
 import Button from "../../components/Button/Button";
 
-const List = ({players = []}) => {
-  console.log('players from List', players)
-  const [firstHalf, setFH] = useState([]); 
-  const [secondHalf, setSH] = useState([]); 
-  
+const List = ({ players = [] }) => {
+  const [firstHalf, setFH] = useState([]);
+  const [secondHalf, setSH] = useState([]);
+
   useEffect(() => {
     const half = Math.ceil(players?.length / 2);
-    setFH(players.splice(0, half))
-    setSH(players.splice(-half))
-
-  }, [])
+    setFH(players.splice(0, half));
+    setSH(players.splice(-half));
+  }, []);
 
   const handleOnClick = () => {
     if (navigator.share) {
@@ -36,40 +34,42 @@ const List = ({players = []}) => {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-around mb-5 text-center">
-        <div className="w-80 bg-white	min-h-80 mx-3 rounded-md divide-y mr-1">
-          <div className="flex justify-center items-center my-2">
-            <TShirt />
-            <h6 className="my-3 font-display font-bold text-3xl ml-1">
-              Equipo 1
-            </h6>
+    <div className="flex flex-col flex-1">
+      <div className="flex-1">
+        <div className="flex justify-around mb-5 text-center ">
+          <div className="w-80 bg-white	min-h-80 mx-3 rounded-md divide-y mr-1">
+            <div className="flex justify-center items-center my-2">
+              <TShirt />
+              <h6 className="my-3 font-display font-bold text-3xl ml-1">
+                Equipo 1
+              </h6>
+            </div>
+            {firstHalf?.map((player, index) => (
+              <p key={index} className="font-display text-2xl h-8 p-1 my-2">
+                {player}
+              </p>
+            ))}
           </div>
-          {firstHalf?.map((player, index) => (
-            <p key={index} className="font-display text-2xl h-8 p-1 my-2">
-              {player}
-            </p>
-          ))}
-        </div>
-        <div className="absolute self-end	">
-          <Versus height={52} width={52} />
-        </div>
-        <div className="w-80 bg-white	min-h-80 mx-3 rounded-md divide-y ml-1">
-          <div className="flex justify-center items-center my-2">
-            <TShirtBlue />
-            <h6 className="my-3 font-display font-bold text-3xl ml-1">
-              Equipo 2
-            </h6>
+          <div className="absolute self-end	">
+            <Versus height={52} width={52} />
           </div>
-          {secondHalf?.map((player,index) => (
-            <p key={index} className="font-display text-2xl h-8 p-1 my-2">{player}</p>
-          ))}
+          <div className="w-80 bg-white	min-h-80 mx-3 rounded-md divide-y ml-1">
+            <div className="flex justify-center items-center my-2">
+              <TShirtBlue />
+              <h6 className="my-3 font-display font-bold text-3xl ml-1">
+                Equipo 2
+              </h6>
+            </div>
+            {secondHalf?.map((player, index) => (
+              <p key={index} className="font-display text-2xl h-8 p-1 my-2">
+                {player}
+              </p>
+            ))}
+          </div>
         </div>
+        <Alert />
       </div>
-      <Alert />
-      <div className="">
-        <Button text={"Compartir"} share={handleOnClick} />
-      </div>
+      <Button text={"Compartir"} share={handleOnClick} />
     </div>
   );
 };
