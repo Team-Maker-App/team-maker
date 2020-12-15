876be2f63130393d88cf2edd95851d714068582a
Hernan Casé <her.case@live.com.ar>
2020-12-14 23:38:15 -0300

    add createTeam feature (#2)
    * add TeamFilter Component
    * add Logo Component
    * add basic styles
    * add Paste function
    * add "Add Player button"
    * add Extra players
    * add Counter is now hidden if player list is empty
    * add separatedByLineAndComma function
    * add randomizePlayers function
    * fix .eslintcache
    * fix Spaces in players array
    * fix SVG incorrect jsx properties
    * fix Players doesnt separate by \n
    * remove .eslintcache

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import TeamFilter from "./components/TeamFilter";
 
 function App() {
   return (
-    <div className="App">
-      <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">TeamMaker</h3>
+    <div className="w-full bg-primaryDark">
+      <div className="container mx-auto h-screen">
+        <TeamFilter />
       </div>
     </div>
   );
