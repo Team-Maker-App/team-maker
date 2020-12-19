1305b60326fb7b004d89b5867905a20233d55053
Sera <30613888+MatiSera@users.noreply.github.com>
2020-12-19 19:37:42 -0300

    Home screen (#6)
    * home screen

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = ({text, share}) => {
+const Button = ({text, action}) => {
   return (
-    <button className="rounded-full py-1 bg-white w-36" onClick={() => share()} >
+    <button className="rounded-full py-1 bg-white w-36 outline-none active:bg-green-700" onClick={() => action()} >
       {text}
     </button>
   );
