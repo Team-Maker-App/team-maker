9f6521a0281ff460809aefc660fad6a304e71a02
Sera <30613888+MatiSera@users.noreply.github.com>
2020-12-20 16:48:27 -0300

    promp of installation (#7)

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,14 +4,26 @@ import Button from "../../components/Button/Button";
 import { ReactComponent as WavyBackground } from "../../styles/svg/curveBackground.svg";
 import { ReactComponent as TeamSVG } from "../../styles/svg/team.svg";
 import { ReactComponent as DarkLogo } from "../../styles/svg/darkLogo.svg";
+import { useReactPWAInstall } from "react-pwa-install";
+
 
 const Home = () => {
   const history = useHistory();
+  const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
 
   const navigate = () => {
     history.push({
       pathname: "/create",
     });
+  };
+
+  const handleInstalation = () => {
+    pwaInstall({
+      title: "Instalar Team Maker",
+      description: "This is a very good app that does a lot of useful stuff. ",
+    })
+      .then(() => alert("Felicidades. Team Maker ya está en tu pantalla de inicio"))
+      .catch(() => console.log("User opted out from installing"));
   };
 
   return (
@@ -22,6 +34,9 @@ const Home = () => {
       </div>
       <div className="z-50 fixed bottom-20">
         <Button text="Crear ya" action={navigate} />
+        {supported() && !isInstalled() && (
+          <Button action={handleInstalation} text="Instalar la app" />
+        )}
       </div>
       <div className="fixed bottom-0 z-0 md:h-82 h-2/6">
         <WavyBackground />
