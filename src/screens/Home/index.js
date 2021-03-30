9b2543385edac225690a66abbe7e652a737b636c
MatiSera <serafinimatias2@gmail.com>
2021-03-30 16:10:34 -0300

    CREATE Homepage desktop & mobile
    * CREATE setps 1,2 & 3 for mobile
    * CREATE HomeScreen for desktop

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,41 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import { ReactComponent as TeamSVG } from "../../styles/svg/team.svg";
 import WavyDivider from "../../components/WavyDivider";
-import { useReactPWAInstall } from "react-pwa-install";
 import Logo from "../../components/Logo";
-import Alert from '../../components/Alert/Alert';
-import Layout from "../../components/Layout";
 
 const Home = () => {
   const history = useHistory();
-  const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
-  const [presentation, setPresentation] = useState(true);
 
   const navigate = () => {
     history.push({
       pathname: "/create",
     });
   };
-
-  const handleInstalation = () => {
-    pwaInstall({
-      title: "Instalar Team Maker",
-      description: "This is a very good app that does a lot of useful stuff. ",
-    })
-      .then(() =>
-        alert("Felicidades. Team Maker ya está en tu pantalla de inicio")
-      )
-      .catch(() => console.log("User opted out from installing"));
-  };
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setPresentation(false)
-    }, 5000);
-    return () => clearTimeout(timer);
-  }, [presentation]);
 
   return (
       <div
@@ -51,28 +28,10 @@ const Home = () => {
         <TeamSVG className="w-3/4" />
       </div>
 
-      <div className="flex flex-col bg-white text-primary font-bold">
+      <div className="flex flex-col bg-white text-primary w-full">
         <WavyDivider />
-        <div className="grid place-items-center w-full flex-1 bg-primary">
-          {presentation ?
-            <p className="text-white text-xl">
-              {`Creá equipos rapidamente con Team Maker \n
-                Los vas a poder compartir en donde quieras \n
-                Comenzá ya mismo apretando en el botón.
-              `}
-            </p> :
-            <>
-              <Button text="Crear ya" action={navigate} />
-              {supported() && !isInstalled() && (
-                <div className="grid place-items-center w-full h-12">
-                  <Button
-                    action={handleInstalation}
-                    text="Instalar la App"
-                  />
-                </div>
-              )}
-            </>
-          }
+        <div className="grid place-items-center flex-1 bg-primary">
+          <Button text="Crear ya" action={navigate} />
         </div>
       </div>
     </div>
