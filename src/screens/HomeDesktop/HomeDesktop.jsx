055117c16790f7f5dfccdf760a2a7e073c4ad789
MatiSera <serafinimatias2@gmail.com>
2021-04-01 13:20:43 -0300

    add eslint fixes

--- src/screens/HomeDesktop/HomeDesktop.jsx
+++ src/screens/HomeDesktop/HomeDesktop.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import {ReactComponent as LogoDesktop} from '../../styles/svg/logo-desktop.svg';
-import {ReactComponent as FormExample} from '../../styles/svg/undraw_text_field_htlv.svg'
-import {ReactComponent as RandomExample} from '../../styles/svg/undraw_random_thoughts_xejj.svg'
-import {ReactComponent as ShareExample} from '../../styles/svg/undraw_Social_media_re_w12q.svg'
-import {ReactComponent as Logo} from '../../styles/svg/isotipo.svg';
+import { useHistory } from 'react-router-dom';
+import { ReactComponent as LogoDesktop } from '../../styles/svg/logo-desktop.svg';
+import { ReactComponent as FormExample } from '../../styles/svg/undraw_text_field_htlv.svg';
+import { ReactComponent as RandomExample } from '../../styles/svg/undraw_random_thoughts_xejj.svg';
+import { ReactComponent as ShareExample } from '../../styles/svg/undraw_Social_media_re_w12q.svg';
+import { ReactComponent as Logo } from '../../styles/svg/isotipo.svg';
 import Button from '../../components/Button/Button';
-import { useHistory } from "react-router-dom";
-
 
 const HomeDesktop = () => {
   const history = useHistory();
 
   const navigate = () => {
     history.push({
-      pathname: "/create",
+      pathname: '/create',
     });
   };
 
@@ -25,15 +24,15 @@ const HomeDesktop = () => {
       <div className="container mx-auto flex flex-row justify-around items-center m-20 flex-1">
         <div className="max-w-min md:w-1/3">
           <FormExample width={402} height={220} />
-          <p className="font-sans text-center text-xl">{"Escribe el nombre de las personas \n que van a participar"}</p>
+          <p className="font-sans text-center text-xl">{'Escribe el nombre de las personas \n que van a participar'}</p>
         </div>
         <div className="max-w-min md:w-1/3">
           <RandomExample width={402} height={220} />
-          <p className="font-sans text-center text-xl">{"Team Maker se encarga\nde mezclarlos aleatoriamente"}</p>
+          <p className="font-sans text-center text-xl">{'Team Maker se encarga\nde mezclarlos aleatoriamente'}</p>
         </div>
         <div className="max-w-min md:w-1/3">
           <ShareExample height={220} width={402} />
-          <p className="font-sans text-center text-xl">{"Comparti el resultado en donde quieras"}</p>
+          <p className="font-sans text-center text-xl">Comparti el resultado en donde quieras</p>
         </div>
       </div>
       <div className="w-full h-60 flex justify-between items-center bg-wavyPattern">
