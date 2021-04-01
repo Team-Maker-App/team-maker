e6482706a2db11c3e066077cb8f55256eebd950f
Sera <30613888+MatiSera@users.noreply.github.com>
2021-04-01 15:45:54 -0300

    fix prod (#14)

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -3,18 +3,16 @@ import PropTypes from 'prop-types';
 import { ReactComponent as Info } from '../../styles/svg/exclamation.svg';
 
 const Alert = ({ text }) => (
-  <div className="mx-auto text-green-500 flex self-center p-2 rounded-md mb-2 bg-green-200 w-5/6">
-    <div className="self-center">
-      <Info width={40} heigth={40} fill="white" />
-    </div>
-    <p className="text-sm pr-3">
-      {text}
-    </p>
-  </div>
+	<div className='mx-auto text-green-500 flex self-center p-2 rounded-md mb-2 bg-green-200 w-5/6'>
+		<div className='self-center'>
+			<Info width={40} heigth={40} fill='white' />
+		</div>
+		<p className='text-sm pr-3'>{text}</p>
+	</div>
 );
 
 Alert.propTypes = {
-  text: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
 };
 
 export default Alert;
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,23 +3,23 @@ import PropTypes from 'prop-types';
 import Logo from '../Logo';
 
 const Layout = ({ children }) => (
-  <div
-    style={{
-      display: 'grid',
-      gridTemplateRows: '80px 1fr',
-      height: '100vh',
-    }}
-    className="background"
-  >
-    <div className="grid place-items-center h-full w-full  bg-primaryDark shadow-xl">
-      <Logo width={200} dark />
-    </div>
-    {children}
-  </div>
+	<div
+		style={{
+			display: 'grid',
+			gridTemplateRows: '80px 1fr',
+			height: '100vh',
+		}}
+		className='background'
+	>
+		<div className='grid place-items-center h-full w-full  bg-primaryDark shadow-xl'>
+			<Logo width={200} dark />
+		</div>
+		{children}
+	</div>
 );
 
 Layout.propTypes = {
-  children: PropTypes.element.isRequired,
+	children: PropTypes.element.isRequired,
 };
 
 export default Layout;
