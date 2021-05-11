6d01456c60389ffe64ad5044148efd5dd2a758ed
Hernan Casé <hernan.case@globant.com>
2021-05-11 15:11:50 -0300

    add handleDelete match players

--- src/screens/Match/index.js
+++ src/screens/Match/index.js
@@ -1,49 +1,102 @@
 import React, { useEffect, useState } from "react";
+
 import { db } from "../../helpers/firebase";
-// Data
+
+import { format } from "date-fns";
+import { es } from "date-fns/esm/locale";
 
 //Components
 import Layout from "../../components/Layout";
 import Feedback from "../../components/Feedback/Feedback";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Match = ({ match }) => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
-  const { location, admin, max_players } = data;
+  const { location, admin, max_players, players, date } = data;
+  const dbMatch = db.collection("matches").doc(match.params.id);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const doc = await db.collection("matches").doc(match.params.id).get();
+    const unsubscribe = dbMatch.onSnapshot((doc) => {
+      setData(doc.data());
+      setLoading(false);
+    });
 
-      if (doc.exists) {
-        setData(doc.data());
-        setLoading(false);
-      } else {
-        console.log("No such document!");
-        setLoading(false);
-      }
-    };
-
-    fetchData();
+    return () => unsubscribe();
   }, []);
+
+  const handleDelete = (selectedPlayer) => {
+    dbMatch.update({
+      players: [...players.filter((player) => player !== selectedPlayer)],
+    });
+  };
 
   return (
     <Layout>
       <div className="flex flex-col p-5 gap-1 max-w-screen-xl mx-auto w-full text-white font-sans">
         {loading && <p>Loading...</p>}
         {!loading && (
-          <>
-            <p>Datos del partido</p>
-            <p>ID: {match.params.id}</p>
-            <p>Ubicación: {location}</p>
-            <p>Admin: {admin}</p>
-            <p>Max: {max_players}</p>
-            <p>Jugadores:</p>
-            <ol className="list-decimal	list-inside	">
-              {data?.players?.map((player) => (
-                <li>{player}</li>
-              ))}
-            </ol>
-          </>
+          <div className="flex flex-col">
+            <div className="screenshot flex flex-col gap-5 p-4 pt-10 pb-16">
+              <div className="col-span-1 flex shadow-sm rounded-md w-full mx-auto">
+                <div className="flex-shrink-0 flex items-center justify-center w-16 bg-purple-600 text-white text-sm font-medium rounded-l-md">
+                  <svg
+                    width={30}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </div>
+                <div className="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md">
+                  <div className="flex-1 px-4 py-2 text-sm">
+                    <p className="text-gray-500">
+                      Ubicación: <span className="text-primary">{location}</span>
+                    </p>
+                    <p className="text-gray-500 capitalize flex gap-1">
+                      Fecha:
+                      <span className="text-primary">
+                        {format(date.toDate(), "EEEE dd/MM - p", {
+                          locale: es,
+                        })}{" "}
+                        hs.
+                      </span>
+                    </p>
+                    <p className="text-gray-500">
+                      Admin: <span className="text-primary">{admin}</span>
+                    </p>
+                    <p className="text-gray-500">
+                      Jugadores: <span className="text-primary">{`${players.length} / ${max_players}`}</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="bg-white shadow overflow-hidden rounded-md">
+                <ul class="divide-y divide-gray-200 text-primary">
+                  {players
+                    ?.filter((_, i) => i < max_players)
+                    .map((player, i) => (
+                      <li key={player} class="flex justify-between items-center h-10">
+                        <span className="pl-6">{`${i + 1}. ${player}`}</span>
+                        <button
+                          onClick={() => handleDelete(player)}
+                          className="flex justify-center items-center h-full w-12 bg-red-200"
+                        >
+                          <FaTrashAlt className="text-red-800" />
+                        </button>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         )}
       </div>
       <div className="w-full text-center p-4 pin-b">
