083b455b1902e321f3958714fc5bcfa8ae9ce92b
Hernan Case <her.case@live.com.ar>
2021-05-12 22:33:39 -0300

    add Responsive Design for Login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -16,7 +16,7 @@ const Login = () => {
   return (
     <Layout>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="mx-6 sm:mx-0 bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -55,7 +55,7 @@ const Login = () => {
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Sign in
+                Ingresar
               </button>
             </div>
           </form>
